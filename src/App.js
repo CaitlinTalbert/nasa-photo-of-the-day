4f06aa52578b02a044e3049f85427abf69323e87
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2021-12-08 15:04:17 -0600

    added axios url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'; 
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', 
+      );
+      setData(result.data); 
+    }; 
+    fetchData();  
+  }, []); 
+
   return (
     <div className="App">
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+            </li>
+        ))}
+      </ul>
+  ); 
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!

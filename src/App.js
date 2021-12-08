8b3d4619718037fb1259fa5910419c68a68c67a9
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2021-12-08 15:20:33 -0600

    added some html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios'; 
+//import axios from 'axios'; 
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState([])
+  /*const [data, setData] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,18 +13,11 @@ function App() {
       setData(result.data); 
     }; 
     fetchData();  
-  }, []); 
+  }, []); */
 
   return (
     <div className="App">
-      <ul>
-        {data.hits.map(item => (
-          <li key={item.objectID}>
-            <a href={item.url}>{item.title}</a>
-            </li>
-        ))}
-      </ul>
-  ); 
+
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
